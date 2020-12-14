4fa806d556aacdc67850d9adbaac11b8c20511b0
heavyweathersystem <neoheavyweather@protonmail.com>
2020-12-14 11:51:47 -0500

    Create api.php

---
+++ includes/api.php
@@ -0,0 +1,106 @@
+<?php
+// error_reporting(E_ERROR | E_WARNING | E_PARSE);
+// This file will act as a API-type thing
+// Include core file
+require_once('core.php');
+
+if (isset($_GET['do'])) {
+    $butt = $_GET['do'];
+
+    switch ($butt) {
+        case "login":
+            login($_POST['email'], $_POST['pass']);
+            break;
+
+        case "register":
+            register($_POST['email'], $_POST['pass'], $_POST['code']);
+            break;
+
+        case "invite":
+            generate($_GET['email'], $_GET['level']);
+            break;
+
+        case "fetch":
+            fetchFiles($_GET['date'], $_GET['count'], $_GET['keyword']);
+            break;
+
+        case "report":
+            report($_GET['file']);
+            break;
+
+        case "mod":
+            mod($_GET['action'], $_GET['date'], $_GET['count'], $_GET['why'], $_GET['file'], $_GET['keyword'], $_GET['fileid'], $_GET['hash'], $_GET['originalname']);
+            break;
+
+        case "cp":
+            header('Location: ../panel');
+            break;
+
+        case "delete":
+            delete($_GET['filename'], $_GET['fileid']);
+            break;
+
+        case "logout":
+            session_unset();
+            session_destroy();
+            session_write_close();
+            header('Location: ../login');
+            break;
+
+        default:
+            echo "We could call this a 404 not found... or what are you doing here?";
+    }
+}
+<?php
+//error_reporting(E_ERROR | E_WARNING | E_PARSE);
+// This file will act as a APIish thing
+// Include core file
+require_once('core.php');
+
+if (isset($_GET['do'])) {
+    $butt = $_GET['do'];
+
+    switch ($butt) {
+        case "login":
+            login($_POST['email'], $_POST['pass']);
+            break;
+
+        case "register":
+            register($_POST['email'], $_POST['pass'], $_POST['code']);
+            break;
+
+        case "invite":
+            generate($_GET['email'], $_GET['level']);
+            break;
+
+        case "fetch":
+            fetchFiles($_GET['date'], $_GET['count'], $_GET['keyword']);
+            break;
+
+        case "report":
+            report($_GET['file']);
+            break;
+
+        case "mod":
+            mod($_GET['action'], $_GET['date'], $_GET['count'], $_GET['why'], $_GET['file'], $_GET['keyword'], $_GET['fileid'], $_GET['hash'], $_GET['originalname']);
+            break;
+
+        case "cp":
+            header('Location: ../panel');
+            break;
+
+        case "delete":
+            delete($_GET['filename'], $_GET['fileid']);
+            break;
+
+        case "logout":
+            session_unset();
+            session_destroy();
+            session_write_close();
+            header('Location: ../login');
+            break;
+
+        default:
+            echo "We could call this a 404 not found... Or what are you doing here?";
+    }
+}
