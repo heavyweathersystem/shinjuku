7515d7140e6b3dfab526fae0d4e17ee7f728c55d
heavyweathersystem <neoheavyweather@protonmail.com>
2020-12-14 19:13:15 -0500

    Create reports.php

---
+++ templates/reports.php
@@ -0,0 +1,11 @@
+<?php include('header.php');?>
+        <p> Status 0 = not removed Status 1 = removed (not shown)</p>
+        <table id="result" class="table"> 
+        <tr>
+            <th>ID</th>
+            <th>File</th>
+            <th>File ID</th>
+            <th>Reporter</th>
+            <th>Status</th>
+            <th>Action</th>
+        </tr>
